0b7eaca77875b5b0629af9dc312f87accc638f60
Yao Xiao <yao@localhost.localdomain>
2021-02-01 18:11:34 +0800

    add mini_get, mini_post functions. Errors are solved

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use reqwest::*;
+
+// #[tokio::main]
+fn main(){
+    mini_get();
+    mini_post();
+}
+
+#[tokio::main]
+async fn mini_get() -> Result<()>{
+    let response = reqwest::get("https://127.0.0.1/8000")
+        .await?
+        .text()
+        .await?;
+    println!("{:?}", response);
+    Ok(())
+}
+
+async fn mini_post() -> Result<()>{
+    let client = reqwest::Client::new();
+    let _res = client.post("https://127.0.0.1/8000")
+        .body("Hello rust")
+        .send()
+        .await?;
+    Ok(())
+}
+
+// async fn mini_put() -> Result<()>{
+//     let client = reqwest::Client::new();
+//     let res = client.put().body("");
+//     Ok(())
+// }
