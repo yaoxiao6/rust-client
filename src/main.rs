2a343ff8d77cb8576b98414387fd7c76a9811c3e
Yao Xiao <yao@localhost.localdomain>
2021-02-01 18:22:58 +0800

    1.changed address from /8000 to :8000. 2. put post ahead of get

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use reqwest::*;
 
 // #[tokio::main]
 fn main(){
-    mini_get();
     mini_post();
+    mini_get();
 }
 
 #[tokio::main]
 async fn mini_get() -> Result<()>{
-    let response = reqwest::get("https://127.0.0.1/8000")
+    let response = reqwest::get("https://127.0.0.1:8000")
         .await?
         .text()
         .await?;
@@ -18,7 +18,7 @@ async fn mini_get() -> Result<()>{
 
 async fn mini_post() -> Result<()>{
     let client = reqwest::Client::new();
-    let _res = client.post("https://127.0.0.1/8000")
+    let _res = client.post("https://127.0.0.1:8000")
         .body("Hello rust")
         .send()
         .await?;
