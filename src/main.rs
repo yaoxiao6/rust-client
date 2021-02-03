0f38a36407c44a7af32778902af0d4e6b29d6990
Yao Xiao <yaoxiao@example.com>
2021-02-03 15:53:16 +0800

    test for setting email

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,54 @@
 use reqwest::*;
+use std::collections::HashMap;
 
-// #[tokio::main]
 fn main(){
-    mini_post();
-    mini_get();
+    // mini_get();
+    // mini_post();
+    mini_json_get();
+    // mini_json_post();
+    println!("Finish running");
 }
 
 #[tokio::main]
 async fn mini_get() -> Result<()>{
-    let response = reqwest::get("https://127.0.0.1:8000")
+    let _response = reqwest::get("http://127.0.0.1:8000")
+    // let response = reqwest::get("https://docs.rs/reqwest/0.9.18/reqwest/index.html")
         .await?
         .text()
-        .await?;
-    println!("{:?}", response);
+        .await?; 
     Ok(())
 }
 
+#[tokio::main]
+async fn mini_json_get() -> Result<()>{
+    let response = reqwest::get("http://127.0.0.1:8000")
+        .await?
+        .json()
+        .await?;
+    println!("body = {:?}", response);    
+    Ok(())
+}
+
+#[tokio::main]
 async fn mini_post() -> Result<()>{
     let client = reqwest::Client::new();
-    let _res = client.post("https://127.0.0.1:8000")
+    let _res = client.post("http://127.0.0.1:8000")
         .body("Hello rust")
+        .send()
+        .await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn mini_json_post() -> Result<()>{
+    // This will POST a body of `{"lang":"rust","body":"json"}`
+    let mut map = HashMap::new();
+    map.insert("lang", "rust");
+    map.insert("body", "json");
+
+    let client = reqwest::Client::new();
+    let _res = client.post("http://127.0.0.1:8000")
+        .json(&map)
         .send()
         .await?;
     Ok(())
